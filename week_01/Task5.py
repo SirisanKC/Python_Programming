bca39b4532bdc2b0a9c218ed15ff0aa0847a0ed9
Sirisan KC <sirisankc74@gmail.com>
2025-05-01 18:07:14 +0300

    Task5 added

---
+++ week_01/Task5.py
@@ -0,0 +1,11 @@
+int1 = int(input("Give the first integer:\n"))
+int2 = int(input("Give the second integer:\n"))
+
+
+print(f"{int1} + {int2} = {int1+int2}")
+print(f"{int1} - {int2} = {int1-int2}")
+print(f"{int1} * {int2} = {int1*int2}")
+print(f"{int1} / {int2} = {int1/int2}")
+print(f"{int1} // {int2} = {int1//int2}")
+print(f"{int1} ** {int2} = {int1**int2}")
+print(f"{int1} % {int2} = {int1%int2}")
