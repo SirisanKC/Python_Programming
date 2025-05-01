67c36de3b9ac9381feec6c6d56bbdbfc7638ebcf
Sirisan KC <sirisankc74@gmail.com>
2025-05-01 18:17:21 +0300

    Task1 added

---
+++ week_02/Task1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:\n")
+int1 = int(input("Enter an integer:\n"))
+float1 = float(input("Enter a float:\n"))
+result = float1**int1
+print(f"Decimal {float1} to power {int1} is {result:.2f}")
+print(f"Thank you for using the program, {name}!")
