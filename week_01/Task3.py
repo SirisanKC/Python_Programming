370345a070dc0dcb744303e56aa8aeac9932e024
Sirisan KC <sirisankc74@gmail.com>
2025-05-01 17:55:51 +0300

    Task3 added

---
+++ week_01/Task3.py
@@ -0,0 +1,5 @@
+product1 = int(input("Enter the price of product 1:\n"))
+product2 = int(input("Enter the price of product 2:\n"))
+
+print(f"Product 1 costs {product1} e and product 2 costs {product2} e.")
+print(f"The sum of the product prices is {product1+product2} euros.")
