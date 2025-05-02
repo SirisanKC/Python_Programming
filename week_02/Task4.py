e3a0a998b06425f5bef425627a34cf7b667dfb8e
Sirisan KC <sirisankc74@gmail.com>
2025-05-02 10:57:56 +0300

    Task4 added

---
+++ week_02/Task4.py
@@ -0,0 +1,11 @@
+int1 = int(input("Enter a positive integer:\n"))
+print(f"Number {int1} multiplied by itself is {int1*int1}")
+
+pi = 3.14
+rad = int(input("Give the radius of a circle as an integer:\n"))
+print(f"The radius of the circle is {rad}, the circumference is {pi*rad} and the area is {pi*rad**2}")
+
+leng = int(input("Enter the length of one side of the rectangle as an integer:\n"))
+bre = int(input("Enter the length of another side of the rectangle as an integer:\n"))
+
+print(f"The sides of the rectangle are {leng} and {bre}; perimeter is {2*leng+bre}; and the area is {leng*bre}.")
