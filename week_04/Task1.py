e2e389cf9dcec45f5c601f9ae0e1059150faf9ed
Sirisan KC <sirisankc74@gmail.com>
2025-05-07 23:02:45 +0300

    Task1 added

---
+++ week_04/Task1.py
@@ -0,0 +1,14 @@
+start = int(input("Enter the starting value:\n"))
+end = int(input("Enter the ending value:\n"))
+
+print("Implementation with a for loop:")
+for i in range (start, end+1):
+    print(i, end= "  ")
+    
+
+print("\nImplementation with a while loop:")
+x = 1;
+while x<=10:
+    print(x, end= "  ")
+    x+=1
+    
