5bf7eeaae34a2f8e906d4c8812a1d2ea15035719
Sirisan KC <sirisankc74@gmail.com>
2025-05-14 14:41:46 +0300

    Task5 added

---
+++ week_10/Task5.py
@@ -0,0 +1,11 @@
+from person import Person
+
+valtteri = Person("Valtteri", 35)
+kimi = Person("Kimi", 45)
+
+
+valtteri.introduce()  
+kimi.introduce() 
+
+kimi.celebrate_birthday()  
+kimi.introduce()  
